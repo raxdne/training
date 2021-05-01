94797f5508cd8bfcc10b6a14e277244ad11146de
A. Tenbusch <raxdne@web.de>
2021-05-01 22:08:24 +0200

    package support added

---
+++ setup.py
@@ -0,0 +1,15 @@
+#
+#
+#
+
+from distutils.core import setup
+
+setup(name='training',
+      version='1.4',
+      description='simple Data Management for Physical Training',
+      author='Alexander Tenbusch',
+      author_email='raxdne@web.de',
+      url='https://github.com/raxdne/training',
+      packages=['training'],
+      # install_requires=['suntime'],
+     )
