e37c248dc8252a17684d37a2cd539dbd76f83f7d
A. Tenbusch <raxdne@web.de>
2021-08-08 19:01:16 +0200

    tag update

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@
 from distutils.core import setup
 
 setup(name='training',
-      version='1.12',
+      version='1.13',
       description='simple Data Management for Physical Training',
       author='Alexander Tenbusch',
       author_email='raxdne@web.de',
