ef491da6feb5dbd341212d89d531815d08a3eb2f
A. Tenbusch <raxdne@web.de>
2022-02-13 20:30:23 +0100

    minor update

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 #
-# Copyright (C) 2021 by Alexander Tenbusch
+# Copyright (C) 2021,2022 by Alexander Tenbusch
 #
 
 from distutils.core import setup
 
 setup(name='training',
-      version='1.16',
+      version='1.17',
       description='simple Data Management for Physical Training',
       author='Alexander Tenbusch',
       author_email='raxdne@web.de',
